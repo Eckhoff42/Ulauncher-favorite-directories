2396cb8b285db57e74b17f5a5020f394bbfcca0e
Eckhoff42 <magnus.wiik.eckhoff@gmail.com>
2022-05-12 14:16:31 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+import json
+import logging
+import os
+from time import sleep
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+
+logger = logging.getLogger(__name__)
+
+
+class DemoExtension(Extension):
+
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+        directories = []
+        logger.info('preferences %s' % json.dumps(extension.preferences))
+
+        # get devices from preferences
+        if extension.preferences.get('directory_list') is not None:
+            directories = extension.preferences['directory_list'].split(',')
+
+            # giv user feedback if no devices has been specified
+            if len(directories) == 0:
+                items.append(ExtensionResultItem(icon='images/dir.png',
+                                                 name="No directories specified",
+                                                 description="Add them in settings->extentions->Directory opener->Directory list",
+                                                 on_enter=ExtensionCustomAction("none", keep_app_open=True)))
+                return RenderResultListAction(items)
+
+        # connect options
+        for i in range(len(directories)):
+            key = directories[i]
+            data = directories[i]
+            items.append(ExtensionResultItem(icon='images/dir.png',
+                                             name="open %s" % key,
+                                             on_enter=ExtensionCustomAction(data, keep_app_open=False)))
+
+        return RenderResultListAction(items)
+
+
+class ItemEnterEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        data = event.get_data()
+        if (data == 'none'):
+            return HideWindowAction()
+
+        root_dir = extension.preferences.get("root_dir")
+        print(f"\n\n\ncalling: nautilus {root_dir}{data}&\n\n\n")
+        # connect to device
+        ret = os.system(
+            f"bash -c 'nautilus {root_dir}{data}&'")
+
+        prompt = "opening" + data
+
+        return RenderResultListAction([ExtensionResultItem(icon='images/dir.png',
+                                                           name=prompt,
+                                                           on_enter=HideWindowAction())])
+
+
+if __name__ == '__main__':
+    DemoExtension().run()
